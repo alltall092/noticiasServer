7870bf6512e55973fd62cbeacd16dca84f0c7a84
julio alberto perez <piyoyo279@gmail.com>
2024-02-05 20:45:10 -0400

    notiicias

--- src/utils/db.js
+++ src/utils/db.js
@@ -2,11 +2,11 @@ const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
 const db = new Sequelize({
-  database: process.env.DB_NAME || "noticias",
-  username: process.env.DB_USER || "julio",
-  host: process.env.DB_HOST || "localhost",
-  //port: process.env.DB_PORT || 5432,
-  password: process.env.DB_PASSWORD || 120786,
+  database: process.env.DB_NAME || "bcko2r2vs07w13gfltyg",
+  username: process.env.DB_USER || "umhsbrzdm8dn6fvx",
+  host: process.env.DB_HOST || "bcko2r2vs07w13gfltyg-mysql.services.clever-cloud.com",
+  port: process.env.DB_PORT || 3306,
+  password: process.env.DB_PASSWORD || "10ser9vkHHMhvXmEz5Kf",
   dialect: "mysql",
   logging: false,
 });
